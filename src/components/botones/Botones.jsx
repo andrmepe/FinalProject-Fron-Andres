5386e50dff086ad5c82ac400add69667b76d380a
haroldCoder <haroldc2005@gmail.com>
2023-04-25 20:01:34 +0930

    update botones 2

--- src/components/botones/Botones.jsx
+++ src/components/botones/Botones.jsx
@@ -4,28 +4,27 @@ import Grid  from '@mui/material/Grid'
 
 const Botones = ()=>{
     return(
-        <Grid container xs={12}>
-            <Grid item={true} md={2.6} xs={8} mt='20px' ><Link to="/InformacionPersonal"style={{marginLeft:'15px', fontSize:'20px', fontFamily:'arial',
+        <Grid container>
+            <Link to="/InformacionPersonal"style={{marginLeft:'40px', fontSize:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Personal information
-                </Link></Grid>
-                <Grid item={true} md={2} xs={8} mt='20px'><Link to="/ProyectosCreados"style={{marginLeft:'15px',fontSize:'20px', fontFamily:'arial',
+                </Link>
+                <Link to="/ProyectosCreados"style={{marginLeft:'20px',fontSize:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Projects created
-                </Link></Grid>
-                <Grid item={true} md={2} xs={12} mt='20px'><Link to="/RedesSociales"style={{marginLeft:'15px', fontSize:'20px', fontFamily:'arial',
+                </Link>
+                <Link to="/RedesSociales"style={{marginLeft:'20px', fontSize:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Social networks
-                </Link></Grid>
-                <Grid item={true} md={2.7} xs={12} mt='20px'><Link to="/TecnologiasAprendidas"style={{marginLeft:'15px', fontSize:'20px', fontFamily:'arial',
+                </Link>
+                <Link to="/TecnologiasAprendidas"style={{marginLeft:'20px', fontSize:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Technologies learned
-                </Link></Grid>
-                <Grid item={true} md={2.7} xs={12} mt='20px'><Link to="/SolicitudesRecibidas"style={{marginLeft:'15px', fontSize:'20px', fontFamily:'arial',
+                </Link>
+                <Link to="/SolicitudesRecibidas"style={{marginLeft:'20px', fontSize:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Applications received
-                </Link></Grid>
-            
+                </Link>
         </Grid>
     )
 }
