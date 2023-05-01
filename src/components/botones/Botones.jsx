e6198318ee27ffdcb6150287100e71211dbd8263
haroldCoder <haroldc2005@gmail.com>
2023-05-01 09:39:24 +0930

    Update responsive

--- src/components/botones/Botones.jsx
+++ src/components/botones/Botones.jsx
@@ -5,23 +5,23 @@ import Grid  from '@mui/material/Grid'
 const Botones = ()=>{
     return(
         <Grid container>
-            <Link to="/InformacionPersonal"style={{marginLeft:'40px', fontSize:'20px', fontFamily:'arial',
+            <Link to="/InformacionPersonal"style={{marginLeft:'40px', marginTop:'20px', fontSize:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Personal information
                 </Link>
-                <Link to="/ProyectosCreados"style={{marginLeft:'20px',fontSize:'20px', fontFamily:'arial',
+                <Link to="/ProyectosCreados"style={{marginLeft:'40px',fontSize:'20px',  marginTop:'20px',fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Projects created
                 </Link>
-                <Link to="/RedesSociales"style={{marginLeft:'20px', fontSize:'20px', fontFamily:'arial',
+                <Link to="/RedesSociales"style={{marginLeft:'40px', fontSize:'20px', marginTop:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Social networks
                 </Link>
-                <Link to="/TecnologiasAprendidas"style={{marginLeft:'20px', fontSize:'20px', fontFamily:'arial',
+                <Link to="/TecnologiasAprendidas"style={{marginLeft:'40px', fontSize:'20px', marginTop:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Technologies learned
                 </Link>
-                <Link to="/SolicitudesRecibidas"style={{marginLeft:'20px', fontSize:'20px', fontFamily:'arial',
+                <Link to="/SolicitudesRecibidas"style={{marginLeft:'40px', fontSize:'20px', marginTop:'20px', fontFamily:'arial',
                 textDecoration:'none', padding:'5px',borderRadius:'5px',border: '1px solid #0afdd7',color: "white"}}>
                 Applications received
                 </Link>
